2ff2da1c7f1e493909ba592882cd5b1ef10ff7df
Soundarya Rajendran <soundaryarj5@gmail.com>
2019-10-18 13:51:05 -0700

    removing review files for mod 2

--- 19-brews-crews/brews-crew/app/models/purchase.rb
+++
@@ -1,8 +0,0 @@
-class Purchase < ApplicationRecord
-  belongs_to :brew
-  validates :size, inclusion: { in: %w(small medium large),
-    message: "%{value} is not a valid size" }
-    validates :creamer_strength, inclusion: { in: %w(small medium large),
-    message: "%{value} is not a valid size" }
-    validates :special_instructions, length: { maximum: 500 }
-end
