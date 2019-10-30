cdb5d292240de06257e417f5dd292ebb2389860f
Hallie Dunn <hal.dunn@flatironschool.com>
2019-10-30 09:19:19 -0700

    valid time solution

--- 99-daily-algorithms/02-valid-time/solution.rb
+++ 99-daily-algorithms/02-valid-time/solution.rb
@@ -1,1 +1,14 @@
-#code solutions here
+#Matt's solution, given the prompt's assumption of valid input
+
+def validTime?(s)
+    res=s.split(':')
+    resI = res.map do |el|
+        el.to_i
+    end
+
+    if resI[0] >= 0 && resI[0] <= 23 && resI[1] >= 0 && resI[1]<= 59
+        return true
+    else
+        return false
+    end
+end
