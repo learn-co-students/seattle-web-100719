d7a7e19d878d7cca06a27d9e490900cd8fc143cb
Hallie Dunn <hal.dunn@flatironschool.com>
2019-10-31 11:29:41 -0700

    smashing stones solution, missing # algorithm

--- 99-daily-algorithms/03-smashing-stones/solution.rb
+++ 99-daily-algorithms/03-smashing-stones/solution.rb
@@ -1,1 +1,20 @@
-#code solution here
+#fonzie's recurisive surprise
+
+def smashing_stones(arr)
+    if arr.length == 0 || arr.length == 1
+        return arr[0]
+    else
+        sorted_arr = arr.sort
+        y = sorted_arr.pop
+        x = sorted_arr.pop
+        if x == y
+            smashing_stones(sorted_arr)
+        else
+            sorted_arr.push(y-x)
+            smashing_stones(sorted_arr)
+        end
+    end
+end
+
+puts smashing_stones([2,4,6,7,3,4,9])
+puts smashing_stones([8,8,8,8,8,8,8,8])
---
+++ 99-daily-algorithms/04-missing-number/README.md
@@ -0,0 +1,16 @@
+# Missing Number
+
+Create a function that takes an array of numbers between 1 and 10 (excluding one number) and returns the missing number.
+
+Example IO:
+```
+missing_num([7, 2, 3, 6, 5, 9, 1, 4, 8])
+=> 10
+```
+
+Notes
+* The array of numbers will be unsorted (not in order).
+* Only one number will be missing.
+
+## Source
+[Code Wars](https://www.codewars.com/kata/find-the-missing-number?utm_source=newsletter&utm_medium=email&utm_campaign=weekly_coding_challenges&utm_term=2019-09-17)
