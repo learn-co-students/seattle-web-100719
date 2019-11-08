a41baaa400cf9c6f396b095a5dc0ef4ae0848a09
Hallie Dunn <hal.dunn@flatironschool.com>
2019-11-08 09:57:44 -0800

    solution for rounding grades

--- 99-daily-algorithms/08-rounding-grades/solution.rb
+++ 99-daily-algorithms/08-rounding-grades/solution.rb
@@ -1,1 +1,17 @@
 #shawn
+
+def round_grades(grades)
+    grades.map do |grade|
+        if grade >= 38
+            if grade%5 >= 3
+                grade += (5-grade%5)
+            else
+              grade
+            end
+        else
+          grade
+        end
+    end
+end
+
+puts round_grades([37, 63, 74, 51])
