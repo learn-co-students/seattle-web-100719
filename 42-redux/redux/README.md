e9ddd4fb5aef8800e915ecae11ef3dd18bc8089d
Hallie Dunn <hal.dunn@flatironschool.com>
2020-01-02 10:20:40 -0800

    basic tree for redux exmaple

---
+++ 42-redux/redux/README.md
@@ -0,0 +1,7 @@
+## Redux Example Lecture
+
+It's an example of redux!
+
+It's a lecutre!
+
+It's...an example of redux in a lecture!
---
+++ 42-redux/redux/src/components/liker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Liker(){
+    return (
+        <>
+        <button>increase</button> <span> <button>decrease</button> </span>
+        </>
+    )
+}
